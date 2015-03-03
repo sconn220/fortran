44ff056a5236a2aaf00245bf698fd314a20b8877
Scott O'Connor <oconn220@msu.edu>
2015-03-03 18:09:08 -0500

    Arnoldi Verified, Matrix dime

--- iter_methods.f90
+++ iter_methods.f90
@@ -2,21 +2,43 @@ program NL_Routines
 
   implicit none
 
-  real(8), Dimension(3,3) :: A,Q,R
-  real(8), Dimension(3) :: x
-  integer :: iters
-  !------QR testing--------!
-  A(1,:) = (/2,4,2/)
-  A(2,:) = (/-1,0,-4/)
-  A(3,:) = (/2,2,-1/)
-  !Q = 0
-  !R = 0
-  !call qr(A,Q,R,3,3)
+  !real(8), Dimension(3,3) :: J
+  !real(8), Dimension(3) :: F,u,delta
+  !integer :: iters
 
-  !------Arnoldi testing----!
-  x = (/1,2,3/)
-  iters = 2
-  call arnoldi(A,x,3,iters)
+  !F = 0.0     !Function Result
+  !J = 0.0     !Jacobian
+  !u = (/0.0,0.0,1.0/)     !Input Variables
+  !delta = 0.0 !inital guess for results
+  !iters = 2
+
+  !call nl_j(J,u)
+  !call nl_f(F,u)
+  !call newton_gmres(F,J,u,delta,3,iters)
+
+  real(8),dimension(5,5) :: A,H,Q_full
+  real(8),dimension(5) :: x
+  integer :: ii,jj
+  H = 0
+  Q_full = 0
+  
+  A(1,:) = (/0.8147,0.0975,0.1576,0.1419,0.6557/)
+  A(2,:) = (/0.9058,0.2785,0.9706,0.4218,0.0357/)
+  A(3,:) = (/0.1270,0.5469,0.9572,0.9157,0.8491/)
+  A(4,:) = (/0.9134,0.9575,0.4854,0.7922,0.9340/)
+  A(5,:) = (/0.6324,0.9649,0.8003,0.9595,0.6787/)
+
+  x = (/0.7577,0.7431,0.3922,0.6555,0.1712/)
+  call arnoldi(A,x,5,3,H,Q_full)
+  
+  !write h to file
+  open (unit = 1, file = 'h.out', status = 'unknown')
+  do ii = 1,5
+      do jj = 1,5
+          write (1,*),ii,jj,H(ii,jj)
+      end do 
+  end do
+  
 end program 
 
 !----------------------------------------------------!
@@ -54,7 +76,7 @@ end program
   
   end subroutine !}}}
 !----------------------------------------------------!
- subroutine arnoldi(A,x,n,iters)!{{{
+ subroutine arnoldi(A,x,n,iters,H,Q_full)!{{{
 
  !--------------------------------------------------!
  !Subroutine: arnoldi - Computes the orthonormal    !
@@ -68,40 +90,39 @@ end program
     
     !Declaration of Variables
     !Input Variables
-    integer,intent(IN) :: n      !Size of matrix A
-    integer,intent(IN) :: iters  !Number of iterations in method
-    real(8), Dimension(n,n), intent(IN) :: A !Matrix A
-    real(8), Dimension(n), intent(IN) :: x   !Initial Guess
+    integer,intent(in) :: n      !Size of matrix A
+    integer,intent(in) :: iters  !Number of iterations in method
+    real(8), Dimension(n,n), intent(in) :: A !Matrix A
+    real(8), Dimension(n), intent(in) :: x   !Initial Guess
     
     !Internal Variables
-    real(8), Dimension(n) :: q   !Current iteration's orthonormal vectors
     real(8), Dimension(n) :: r   !
     
     !Output Variables
-    real(8), Dimension(n+1,n) :: H          !Hessenberg Matrix
+    real(8), Dimension(n,n) :: H          !Hessenberg Matrix
     real(8), Dimension(n,iters) :: Q_full !Set of all Orthonormal Vecotrs
     
-    if (iters >= n) then 
+    if (iters .gt. n) then 
         print*,'Error: Number of iteration exceeds matrix size'
     else
-        q = x/norm2(x)
-        Q_full(:,1) = q
+        Q_full(:,1) = x/norm2(x)
         !Number of iteration to solve
         do jj = 1,iters
-            r = matmul(A,q)
+            r = matmul(A,Q_full(:,jj))
             !Stablized Gram-Schmidt Orthogonalization
             do ii = 1,jj
-                H(ii,jj) = dot_product(q,r)
-                r = r - q*H(ii,jj)
+                H(ii,jj) = dot_product(Q_full(:,ii),r)
+                r = r - H(ii,jj)*Q_Full(:,ii)
             end do
-            H(jj+1,jj) = norm2(r)
-            Q_full(:,jj) = q
-            q = r/H(jj+1,jj)
+            if (jj .lt. n) then 
+                H(jj+1,jj) = norm2(r)
+                Q_full(:,jj+1) = r/H(jj+1,jj) 
+            end if 
         end do
     end if
  end subroutine!}}}
 !----------------------------------------------------!
-subroutine newton_gmres(F,X)!{{{
+subroutine newton_gmres(F,J,x,delta,n,iters)!{{{
  !----------------------------------------------------------
  !Subroutine: Generalized Minimal Residual Method (GMRES)
  !            Approximates solution to a nonsymmetrix
@@ -115,19 +136,53 @@ subroutine newton_gmres(F,X)!{{{
  !       J - Jacobian
  !       X - inital condition 
  !       delta - Inital Guess
- !       R - Residue
+ !       n - size of F
  !
- !Ouput:
+ !Ouput: Y - Aproximate solution to current time step.
  !----------------------------------------------------------
-  
+
   !Declear Variables
+  !Input Variables
+  real(8),dimension(n,n),intent(in) :: J   !Function and Jacobian
+  real(8),dimension(n),intent(in) :: F,x   !Function, Input Variables
+  real(8),dimension(n),intent(in) :: delta !Guess to solution
   
+  !Internal Variables
+  real(8),dimension(n) :: r !Residue 
+  real(8),dimension(n,iters) :: Q_Full
+  real(8),dimension(n+1,n) :: H
   !Initiate Variables
-
-  !Arnoldi Process
-
+  r = 0
+  Q_Full = 0
+  H = 0 
+  !Arnoldi Process:
+  r = -F-matmul(J,delta)  !compute residue
+  call arnoldi(J,r,3,iters,H,Q_Full)
+  print*, H
   !Least Squares Using QR Process
 
 
 end subroutine!}}}
 !----------------------------------------------------!
+subroutine nl_j(J,u) !{{{
+  real(8),Dimension(3,3) :: J
+  real(8),Dimension(3) :: u
+  
+  J(1,1) = 0.001
+  J(1,2) = -0.001
+  J(1,3) = 1.0
+  J(2,1) = -0.001
+  J(2,2) = 0.001+0.001*3.0*u(2)**2
+  J(2,3) = 0.0
+  J(3,1) = 1.0
+  J(3,2) = 0.0
+  J(3,3) = 0.0
+
+end subroutine !}}}
+!----------------------------------------------------!
+subroutine nl_f(F,u) !{{{
+  real(8),Dimension(3):: F,u
+  F(1) = 0.001*(u(1)-u(2))+u(3)
+  F(2) = 0.001*(u(2)-u(1))+0.001*(u(2)**3)
+  F(3) = u(3)-1
+end subroutine !}}}
